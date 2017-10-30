398d09c21d56a80bae4c40590078dedb3aab7799
Scott Basgaard <sbasgaard@gmail.com>
2017-10-30 20:37:40 +0100

    Add test job.

---
+++ services/activity.service.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const QueueService = require( "moleculer-bull" );
+
+module.exports = {
+	name: "activity",
+	mixins: [ QueueService() ],
+
+	/**
+	 * Default settings
+	 */
+	settings: {},
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Log activity
+		 *
+		 * @returns
+		 */
+		log() {
+			this.testJob()
+			return "Log";
+		},
+	},
+
+	/**
+	 * Events
+	 */
+	events: {},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+		testJob() {
+			this.createJob( "job.send", { id: id++, pid: process.pid } );
+
+			this.getQueue( "job.send" ).on( "global:progress", ( jobID, progress ) => {
+				this.logger.info( `Job #${jobID} progress is ${progress}%` );
+			} );
+
+			this.getQueue( "job.send" ).on( "global:completed", ( job, res ) => {
+				this.logger.info( `Job #${job.id} completed!. Result:`, res );
+			} );
+		}
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	stopped() {
+
+	}
+};
