280612ee6c235a1429cf7a34205e774039f6d6a6
Scott Basgaard <sbasgaard@gmail.com>
2017-10-30 21:21:49 +0100

    Test failures.

--- test-cluster.js
+++ test-cluster.js
@@ -1,29 +1,35 @@
-var
-	Queue = require( 'bull' ),
-	cluster = require( 'cluster' );
+var Queue = require( 'bull' )
+var cluster = require( 'cluster' );
 
-var numWorkers = 8;
+var numWorkers = 4;
 var queue = new Queue( "test concurrent queue" );
 
 if ( cluster.isMaster ) {
+
 	for ( var i = 0; i < numWorkers; i ++ ) {
 		cluster.fork();
 	}
 
 	cluster.on( 'online', function ( worker ) {
+
 		// Lets create a few jobs for the queue workers
 		for ( var i = 0; i < 500; i ++ ) {
 			queue.add( { foo: 'bar' } );
 			console.log( "Job added " + i );
 		}
-		;
 	} );
 
 	cluster.on( 'exit', function ( worker, code, signal ) {
 		console.log( 'worker ' + worker.process.pid + ' died' );
 	} );
 } else {
+
 	queue.process( function ( job, jobDone ) {
+
+		if ( 1 === cluster.worker.id ) {
+			throw new Error('some unexpected error');
+		}
+
 		console.log( "Job done by worker", cluster.worker.id, job.id );
 		jobDone();
 	} );
