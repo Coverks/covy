b6945c26ac30de89bfb2aac640026b15a36fc071
Scott Basgaard <sbasgaard@gmail.com>
2017-10-30 20:42:54 +0100

    More tests.

--- services/activity.service.js
+++ services/activity.service.js
@@ -37,7 +37,8 @@ module.exports = {
 	 */
 	methods: {
 		testJob() {
-			this.createJob( "job.send", { id: id++, pid: process.pid } );
+			var id = Math.floor((Math.random() * 2000) + 1);
+			this.createJob( "job.send", { id: i, pid: process.pid } );
 
 			this.getQueue( "job.send" ).on( "global:progress", ( jobID, progress ) => {
 				this.logger.info( `Job #${jobID} progress is ${progress}%` );
