6d59334070edbd3db8d9221e38d8531d341c3af5
Scott Basgaard <sbasgaard@gmail.com>
2017-10-30 21:07:14 +0100

    Another bull test.

--- services/activity.service.js
+++ services/activity.service.js
@@ -1,8 +1,5 @@
 "use strict";
 
-const Queue = require( 'bull' );
-const cluster = require( 'cluster' );
-
 module.exports = {
 	name: "activity",
 
@@ -22,7 +19,6 @@ module.exports = {
 		 * @returns
 		 */
 		log() {
-			this.testJob()
 			return "Log";
 		},
 	},
@@ -36,33 +32,6 @@ module.exports = {
 	 * Methods
 	 */
 	methods: {
-		testJob() {
-			var numWorkers = 8;
-			var queue = new Queue( "test concurrent queue" );
-
-			if ( cluster.isMaster ) {
-				for ( var i = 0; i < numWorkers; i ++ ) {
-					cluster.fork();
-				}
-
-				cluster.on( 'online', function ( worker ) {
-					// Lets create a few jobs for the queue workers
-					for ( var i = 0; i < 500; i ++ ) {
-						queue.add( { foo: 'bar' } );
-					}
-					;
-				} );
-
-				cluster.on( 'exit', function ( worker, code, signal ) {
-					console.log( 'worker ' + worker.process.pid + ' died' );
-				} );
-			} else {
-				queue.process( function ( job, jobDone ) {
-					console.log( "Job done by worker", cluster.worker.id, job.id );
-					jobDone();
-				} );
-			}
-		}
 	},
 
 	/**
