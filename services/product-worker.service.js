9392f2cc1f14d0cd46a4e155abe27c29da673268
Scott Basgaard <sbasgaard@gmail.com>
2017-11-02 11:18:32 +0000

    Commit for Dekode meeting.

--- services/product-worker.service.js
+++ services/product-worker.service.js
@@ -1,7 +1,17 @@
 "use strict";
 
+let config = require('../config');
 const QueueService = require("moleculer-bull");
 const sleep = require('sleep');
+const WooCommerceAPI = require('woocommerce-api');
+
+let WooCommerce = new WooCommerceAPI({
+	url: config.sites.test.url,
+	consumerKey: config.sites.test.key,
+	consumerSecret: config.sites.test.secret,
+	wpAPI: true,
+	version: 'wc/v1'
+});
 
 module.exports = {
 
@@ -13,22 +23,53 @@ module.exports = {
 		"products.task"(job) {
 
 			this.logger.info("New job received!");
-			const products = job.data;
-			const productsLength = Object.keys(products).length;
 
+			const product = job.data;
+			const productLength = Object.keys(product).length;
+
+			let productID = 8;
+
+			/*
+			if ( product.hasOwnProperty('sku') ) {
+
+				console.log( product.sku );
+
+				WooCommerce.get(`products/?filter[sku]=${product.sku}`, function(err, data, res) {
+					let productResult = res;
+					productResult = JSON.parse( productResult );
+					productID = productResult[0].id;
+				});
+
+				delete product.sku;
+
+			} else {
+				// @TODO: Create product here.
+			}
+			*/
+
+			let data = {};
 			let i = 1;
 
-			Object.keys(products).forEach(function( key ) {
-				job.progress( Math.round( ( i /productsLength ) * 100 ) );
+			Object.keys(product).forEach(function( k ) {
+				// job.progress( Math.round( ( i /productLength ) * 100 ) );
+				data[k]  = String(product[k]);
 				i++;
 			} );
 
-			return new this.Promise(resolve => {
-				setTimeout(() => resolve({
-					done: true,
-					id: job.data.id,
-					worker: process.pid
-				}), 3000);
+			// console.log( productID );
+			// console.log( data );
+
+			let endpoint = `products/${productID}`;
+			// console.log( endpoint );
+
+			WooCommerce.put(endpoint, data, function(err, data, res) {
+				// console.log(res);
+			});
+
+			return this.Promise.resolve({
+				done: true,
+				id: job.data.id,
+				worker: process.pid
 			});
 		}
 	}
